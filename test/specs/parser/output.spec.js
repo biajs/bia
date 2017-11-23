ef375ad446a6dce16a7ea20a53e157244e4b4eac
scottbedard <scottbedard90@gmail.com>
2017-11-22 22:39:11 -0700

    parse a text node

--- test/specs/parser/output.spec.js
+++ test/specs/parser/output.spec.js
@@ -1,12 +1,12 @@
 const { parse } = require('../../../');
 
-describe('template parser output', () => {
+describe.only('template parser output', () => {
 
     // helper function to parse fixtures
-    const parseFixture = f => parse(getFileContents(f, { filename: 'foo.bia' }));
+    const parseFixture = f => parse(getFileContents(f, { filename: 'foo.bia' })).template;
      
-    it('element_with_text', () => {
-        expect(parseFixture('test/fixtures/element_with_text.bia').template).to.deep.equal({
+    it('parses an element with text', () => {
+        expect(parseFixture('test/fixtures/element_with_text.bia')).to.deep.equal({
             tagName: 'DIV',
             type: 'ELEMENT',
             children: [
@@ -14,8 +14,39 @@ describe('template parser output', () => {
                     children: [],
                     type: 'TEXT',
                     tagName: undefined,
-                }
-            ]
+                },
+            ],
         });
+    });
+
+    it('parses an element with text and child element', () => {
+        expect(parseFixture('test/fixtures/element_with_text_and_child.bia')).to.deep.equal({
+            children: [
+                {
+                    children: [],
+                    tagName: undefined,
+                    type: 'TEXT',
+                },
+                {
+                    children: [
+                        {
+                            //
+                            children: [],
+                            tagName: undefined,
+                            type: 'TEXT',
+                        }
+                    ],
+                    tagName: 'SPAN',
+                    type: 'ELEMENT',
+                },
+                {
+                    children: [],
+                    tagName: undefined,
+                    type: 'TEXT',
+                }    
+            ],            
+            tagName: 'DIV',
+            type: 'ELEMENT',
+        })
     });
 });
