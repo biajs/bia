94cb5af974e91dbc2a555c9cb54f22fc23216544
scottbedard <scottbedard90@gmail.com>
2017-12-28 00:52:52 -0700

    rename file

---
+++ test/specs/compilation/nested_if_block/test.ts
@@ -0,0 +1,10 @@
+import { compile, div, expect, render } from '../../../utils';
+
+export default function(file) {
+    it.skip('nested_if_block', () => {
+        // const { code } = compile(file);
+        // const vm = render(file, { el: div() });
+
+        // console.log(vm.$el.outerHTML);
+    });
+}
