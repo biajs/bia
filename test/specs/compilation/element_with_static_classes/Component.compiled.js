8335f3d9de4f1c09f20b48d78159b88eb74e7fa5
Scott Bedard <scottbedard90@gmail.com>
2017-12-29 09:02:51 -0700

    recompile fixture:

--- test/specs/compilation/element_with_static_classes/Component.compiled.js
+++ test/specs/compilation/element_with_static_classes/Component.compiled.js
@@ -19,25 +19,25 @@ function createElement(tag) {
 }
 
 function create_main_fragment(vm) {
-    var div, span;
+    var div, p;
 
-    var if_block_1 = (false) && create_if_block_1(vm);
+    var if_block_2 = (true) && create_if_block_2(vm);
     return {
         c: function create() {
             div = createElement('div');
-            if (if_block_1) if_block_1.c();
-            span = createElement('span');
+            p = createElement('p');
+            if (if_block_2) if_block_2.c();
             this.h();
             return div;
         },
         d: noop,
         h: function hydrate() {
-            span.className = 'foo bar';
+            p.className = 'bar';
         },
         m: function mount(target, anchor) {
             insertNode(div, target, anchor);
-            if (if_block_1) if_block_1.m(div, null);
-            appendNode(span, div);
+            appendNode(p, div);
+            if (if_block_2) if_block_2.m(div, null);
         },
         p: noop,
         u: function unmount() {
@@ -46,12 +46,13 @@ function create_main_fragment(vm) {
     };
 }
 
-function create_if_block_1(vm) {
+function create_if_block_2(vm) {
     var p;
 
     return {
         c: function create() {
             p = createElement('p');
+            p.innerHTML = '\r\n            <span class=\"foo\"></span>\r\n        ';
             return p;
         },
         d: noop,
