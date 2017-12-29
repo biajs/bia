26f743b6857c4c46ba1d2e976be3bff0a3fbb147
Scott Bedard <scottbedard90@gmail.com>
2017-12-29 08:01:09 -0700

    compile fixtures

--- test/specs/compilation/element_with_nested_static_and_dynamic_children/Component.compiled.js
+++ test/specs/compilation/element_with_nested_static_and_dynamic_children/Component.compiled.js
@@ -2,6 +2,18 @@
 
 function noop() {}
 
+function appendNode(node, target) {
+    target.appendChild(node);
+}
+
+function createText(text) {
+    return document.createTextNode(text);
+}
+
+function setText(el, text) {
+    el.textContent = text;
+}
+
 function detachNode(node) {
     node.parentNode.removeChild(node);
 }
@@ -15,20 +27,47 @@ function createElement(tag) {
 }
 
 function create_main_fragment(vm) {
-    var div;
+    var div, text, div_1;
 
+    var if_block_1 = (true) && create_if_block_1(vm);
     return {
         c: function create() {
             div = createElement('div');
-            div.innerHTML = '';
+            if (if_block_1) if_block_1.c();
+            text = createText('\r\n        static text\r\n        ');
+            div_1 = createElement('div');
+            div_1.innerHTML = '\r\n            nested static text\r\n            <p>nested static element</p>\r\n        ';
             return div;
         },
         d: noop,
         m: function mount(target, anchor) {
             insertNode(div, target, anchor);
+            if (if_block_1) if_block_1.m(div, null);
+            appendNode(text, div);
+            appendNode(div_1, div);
         },
         u: function unmount() {
             detachNode(div);
+        },
+        p: noop
+    };
+}
+
+function create_if_block_1(vm) {
+    var span;
+
+    return {
+        c: function create() {
+            span = createElement('span');
+            setText(span, 'dynamic');
+            return span;
+        },
+        d: noop,
+        m: function mount(target, anchor) {
+            insertNode(span, target, anchor);
+        },
+        u: function unmount() {
+            detachNode(span);
         },
         p: noop
     };
