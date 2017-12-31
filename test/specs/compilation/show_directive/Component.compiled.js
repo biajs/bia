aed1270b563ed5f3f3f207a289c9bcbc26329e1d
scottbedard <scottbedard90@gmail.com>
2017-12-31 09:17:12 -0700

    compile fixtures

--- test/specs/compilation/show_directive/Component.compiled.js
+++ test/specs/compilation/show_directive/Component.compiled.js
@@ -2,6 +2,19 @@
 
 function noop() {}
 
+function toggleVisibility(el, isVisible) {
+    if (isVisible) el.style.removeProperty('display');
+    else el.style.setProperty('display', 'none');
+}
+
+function appendNode(node, target) {
+    target.appendChild(node);
+}
+
+function setText(el, text) {
+    el.textContent = text;
+}
+
 function detachNode(node) {
     node.parentNode.removeChild(node);
 }
@@ -15,17 +28,27 @@ function createElement(tag) {
 }
 
 function create_main_fragment(vm) {
-    var div;
+    var div, p, span;
 
     return {
         c: function create() {
             div = createElement('div');
+            p = createElement('p');
+            setText(p, 'hidden');
+            span = createElement('span');
+            setText(span, 'visible');
+            this.h();
             return div;
         },
         d: noop,
-        h: noop,
+        h: function hydrate() {
+            toggleVisibility(p, false);
+            toggleVisibility(span, true);
+        },
         m: function mount(target, anchor) {
             insertNode(div, target, anchor);
+            appendNode(p, div);
+            appendNode(span, div);
         },
         p: noop,
         u: function unmount() {
