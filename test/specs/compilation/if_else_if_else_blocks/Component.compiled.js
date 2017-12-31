23785442046bdf1a6cc1f26b09f3175e57e9af9d
scottbedard <scottbedard90@gmail.com>
2017-12-31 08:05:15 -0700

    compile fixtures

--- test/specs/compilation/if_else_if_else_blocks/Component.compiled.js
+++ test/specs/compilation/if_else_if_else_blocks/Component.compiled.js
@@ -2,6 +2,10 @@
 
 function noop() {}
 
+function setText(el, text) {
+    el.textContent = text;
+}
+
 function detachNode(node) {
     node.parentNode.removeChild(node);
 }
@@ -17,19 +21,86 @@ function createElement(tag) {
 function create_main_fragment(vm) {
     var div;
 
+    var current_block_type = select_block_type(vm);
+    var if_block = current_block_type(vm);
     return {
         c: function create() {
             div = createElement('div');
+            if_block.c();
             return div;
         },
         d: noop,
         h: noop,
         m: function mount(target, anchor) {
             insertNode(div, target, anchor);
+            if_block.m(div, null);
         },
         p: noop,
         u: function unmount() {
             detachNode(div);
+        }
+    };
+}
+
+function create_else_block(vm) {
+    var p;
+
+    return {
+        c: function create() {
+            p = createElement('p');
+            setText(p, '3');
+            return p;
+        },
+        d: noop,
+        h: noop,
+        m: function mount(target, anchor) {
+            insertNode(p, target, anchor);
+        },
+        p: noop,
+        u: function unmount() {
+            detachNode(p);
+        }
+    };
+}
+
+function create_else_if_block(vm) {
+    var p;
+
+    return {
+        c: function create() {
+            p = createElement('p');
+            setText(p, '2');
+            return p;
+        },
+        d: noop,
+        h: noop,
+        m: function mount(target, anchor) {
+            insertNode(p, target, anchor);
+        },
+        p: noop,
+        u: function unmount() {
+            detachNode(p);
+        }
+    };
+}
+
+function create_if_block(vm) {
+    var p;
+
+    return {
+        c: function create() {
+            p = createElement('p');
+            setText(p, '1');
+            return p;
+        },
+        d: noop,
+        h: noop,
+        m: function mount(target, anchor) {
+            insertNode(p, target, anchor);
+        },
+        p: noop,
+        u: function unmount() {
+            detachNode(p);
         }
     };
 }
@@ -41,6 +112,12 @@ function Component(options) {
         this.$el = fragment.c();
         fragment.m(options.el, options.anchor || null);
     }
+}
+
+function select_block_type(vm) {
+    if (false) return create_if_block;
+    if (true) return create_else_if_block;
+    return create_else_block;
 }
 
 export default Component;
