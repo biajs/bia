fcb12f8a263dfa7115922479a5554fa011952491
Scott Bedard <scottbedard90@gmail.com>
2018-01-12 07:58:13 -0700

    flesh out loop test a bit

--- test/specs/compilation/for_loop/test.ts
+++ test/specs/compilation/for_loop/test.ts
@@ -8,11 +8,14 @@ export default function(file) {
         const vm = render(file, { 
             el: div(),
             data: {
-                bar: [1, 2, 3],
+                people: ['Jim', 'Mary', 'Bob'],
             },
         });
 
         // @todo: make assertion that each text node is correct
-        expect(vm.$el.querySelectorAll('span').length).to.equal(3);
+        expect(vm.$el.querySelectorAll('li').length).to.equal(3);
+        expect(vm.$el.querySelector('li:nth-child(1)').textContent.trim()).to.equal('name: Jim');
+        expect(vm.$el.querySelector('li:nth-child(2)').textContent.trim()).to.equal('name: Mary');
+        expect(vm.$el.querySelector('li:nth-child(3)').textContent.trim()).to.equal('name: Bob');
     });
 }
