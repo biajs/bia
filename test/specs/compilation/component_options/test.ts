715d8c77e8c9679dc3f34810c82fd8218370b693
Scott Bedard <scottbedard90@gmail.com>
2018-01-15 08:18:25 -0700

    fix ts error

--- test/specs/compilation/component_options/test.ts
+++ test/specs/compilation/component_options/test.ts
@@ -2,8 +2,8 @@ import { compile, div, expect, render } from '../../../utils';
 
 export default function(file) {
     it('component_options', function() {
-        const { code } = compile(file);
-        console.log(code);
+        const result = compile(file);
+        console.log(result);
 
         // const el = div();
         // const vm = render(file, { el, foo: 'bar' });
