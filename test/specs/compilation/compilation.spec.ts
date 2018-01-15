4af35891ea8bf4ba0212c66d32c6e8f802388582
Scott Bedard <scottbedard90@gmail.com>
2018-01-15 07:26:28 -0700

    cleanup log

--- test/specs/compilation/compilation.spec.ts
+++ test/specs/compilation/compilation.spec.ts
@@ -1,14 +1,11 @@
 describe('compilation', function() {
     const fs = require('fs');
     const path = require('path');
-
     const currentPath = path.resolve(__dirname);
     const dirs = fs.readdirSync(currentPath).filter(f => fs.statSync(path.join(currentPath, f)).isDirectory());
 
     dirs.forEach(dir => {
         const testDir = `${currentPath}/${dir}`;
-
-        console.log('test dir', testDir);
         
         require(`${testDir}/test`).default(`${testDir}/Component.bia`);
     });
