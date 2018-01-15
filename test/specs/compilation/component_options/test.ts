94b46384abbaf2063dea4093cbbab1a89b37155b
Scott Bedard <scottbedard90@gmail.com>
2018-01-15 08:10:58 -0700

    try to catch the error

--- test/specs/compilation/component_options/test.ts
+++ test/specs/compilation/component_options/test.ts
@@ -1,9 +1,16 @@
 import { compile, div, expect, render } from '../../../utils';
 
 export default function(file) {
-    it('component_options', function() {
-        const { code } = compile(file);
-        console.log(code);
+    it('component_options', function(done) {
+        try {
+
+            const { code } = compile(file);
+            console.log(code);
+            setTimeout(done, 1500);
+        } catch(e) {
+            console.log ('err', e);
+            done();
+        }
 
         // const el = div();
         // const vm = render(file, { el, foo: 'bar' });
